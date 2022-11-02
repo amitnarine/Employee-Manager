729dd3f37b0055aeaffa634162e042e720160b51
amitnarine <57597739+amitnarine@users.noreply.github.com>
2022-11-02 16:54:43 -0400

    added transactional to fix delete method in back end

--- src/main/java/com/amitnarine/employeemanager/EmployeeResource.java
+++ src/main/java/com/amitnarine/employeemanager/EmployeeResource.java
@@ -4,6 +4,7 @@ import com.amitnarine.employeemanager.model.Employee;
 import com.amitnarine.employeemanager.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -41,6 +42,7 @@ public class EmployeeResource {
         Employee updateEmployee = employeeService.updateEmployee(employee);
         return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
     }
+    @Transactional
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
         employeeService.deleteEmployee(id);
